41cb68bc92ebf278c8944eaa6fd3de66ee479589
Aishwarya Biradar <121895009+AishwaryGB@users.noreply.github.com>
2024-12-17 00:38:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,109 +1,42 @@
-﻿namespace oops_Abstract
+﻿namespace oops_collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region cmath add and adv obj
-            //cmath cmath = new cmath();
-            //Console.WriteLine("enter first number");
-            //int x = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("enter the sec number");
-            //int y = Convert.ToInt32(Console.ReadLine());
-            //int result = cmath.add(x, y);
-            //Console.WriteLine("add" + result.ToString());
+            CMath obj = new CMath();
+            int p = 10;
+            int q = 20;
+            Console.WriteLine("Before Swap : P ={0} , Q = {1}", p, q);
 
-            //advmath advmath = new advmath();
-            //Console.WriteLine(advmath.square(5));
-            //Console.WriteLine(advmath.advclassadd(5, 5)); 
-            #endregion
-            demo2 dem=new demo2();
-            dem.saybye();
-            dem.sayhi();
-        
+            //to pass arguments to a method there are two ways:
+            // obj.Swap(p, q); //1. Pass by Value
+            //2. Pass by refernce
+            obj.swap(ref p, ref q);
+            Console.WriteLine("After Swap : P ={0} , Q = {1}", p, q);
+
+            //out parameter
+            int sum, product;
+            obj.Calculate(2, 3, out sum, out product);
+            Console.WriteLine($"Sum :{sum} , Product:{product} ");
         }
     }
-
-    public class demo1
+    public class CMath //Generic Class
     {
-        public virtual void sayhi()
+        public void swap(ref int x, ref int y) //Generic Method
         {
-            Console.WriteLine("hello");
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
         }
-
-        public virtual void saygreet()
+        public void Calculate(int x, int y, out int sum, out int product)
         {
-            Console.WriteLine("hello world");
 
-        }
 
-        public void saybye()
-        {
-            Console.WriteLine("bye");
-        }
 
-        
-    }
-    public class demo2 :demo1
-    {
-        public override void sayhi()
-        {
-            Console.WriteLine("hello aish");
-        }
-        public new void saybye()
-        {
-            Console.WriteLine("bye bye");
+            sum = x + y;
+            product = x * y;
         }
     }
-        public class cmath
-        {
-            private int _num1;
-            public cmath()
-            {
-
-            }
-            public cmath(int para1)
-            {
-                _num1 = para1;
-            }
-            public int add(int x, int y)
-            {
-                return x + y;
-
-            }
-            public int sub(int x, int y)
-            {
-                return x - y;
-            }
-
-
-        }
-        public class advmath : cmath
-        {
-            private int num2;
-
-            public advmath()
-            {
-
-            }
-            public advmath(int num2, int para) : base(para)
-            {
-                this.num2 = num2;
-            }
-            public int square(int x)
-            {
-                return x * x;
-            }
-            public int sub(int x, int y, int p)
-            {
-                return x - y + p;
-            }
-
-            public int advclassadd(int x, int y)
-            {
-                int result = base.add(x, y);
-                return result;
-            }
-        }
-    }
-
+}
